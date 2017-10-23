2f63450574f7d2dc44039f9d00f787a7d140bc87
ITA-GRAM-W-43\rgergana <rusenova>
2017-10-23 17:07:36 +0200

    updated server code

--- 07_ur_online/server/server_localnetwork.py
+++ 07_ur_online/server/server_localnetwork.py
@@ -1,14 +1,10 @@
 import json
-import logging
 ## for python 2:
 #from urlparse import urlparse, parse_qs
 #from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 ## for python 3:
 from urllib.parse import urlparse, parse_qs
 from http.server import BaseHTTPRequestHandler, HTTPServer
-
-logging.basicConfig(level=logging.DEBUG)
-LOG = logging.getLogger()
 
 DATA = {'message_to_send': 0}
 
@@ -25,26 +21,18 @@ def build_handler():
             if 'message' in qs and len(qs['message']):
                 #DATA['message_to_send'] = int(qs['message'][0])
                 DATA['message_to_send'] = str(qs['message'][0])
-            self.wfile.write(json.dumps(DATA['message_to_send'],
-            ensure_ascii=False).encode('utf8'))
+            self.wfile.write(json.dumps(DATA['message_to_send']).encode('utf8'))
 
     return LayerDataHandler
 
 if __name__ == '__main__':
-    from sys import argv
-
-    if len(argv) > 1:
-        http_port = int(argv[1])
-    else:
-        http_port = 5000
-
-    httpd = HTTPServer(('0.0.0.0', http_port), build_handler())
-    LOG.info('Server configured on port %d, for a different port, start with: python server.py [http_port]', http_port)
-    LOG.info('Starting http server, use <Ctrl-C> to stop...')
+    httpd = HTTPServer(('0.0.0.0', 5000), build_handler())
+    print ('Server configured on port 5000')
+    print ('Starting http server, use <Ctrl-C> to stop...')
 
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
         pass
 
-    LOG.info('Exiting...')
+    print ('Exiting...')
